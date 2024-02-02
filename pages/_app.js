e96f70fbf390c2519c6eb2481d695cf1c106524a
Sophia <151823139+SophiaZett@users.noreply.github.com>
2024-02-02 17:55:14 +0100

    fail

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
 import GlobalStyle from "../styles";
 import { initialTransactions } from "@/ressources/data";
 import { useState } from "react";
+import Form from "@/components/Form/Form";
 
 export default function App({ Component, pageProps }) {
   const [transactions, setTransactions] = useState(initialTransactions);
-  const addTransaction = (newTransaction) => {
+
+  function addTransaction(newTransaction) {
     setTransactions([...transactions, newTransaction]);
-  };
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    console.log("Formdata: ", formData);
+  }
+
   return (
     <>
       <GlobalStyle />
@@ -15,6 +26,7 @@ export default function App({ Component, pageProps }) {
         initialTransactions={initialTransactions}
         addTransaction={addTransaction}
       />
+      <Form handleSubmit={handleSubmit} />
     </>
   );
 }
