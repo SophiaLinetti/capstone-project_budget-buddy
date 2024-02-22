4c4e9e6d9ca20753a65f070da4bfbbcfa64fea56
Eileen Ilsemann <eileenilsemanni@gmail.com>
2024-02-22 13:05:46 +0100

    Create Dropdown Menu for Categpry Filter

---
+++ components/FilterCategory/FilterCategory.js
@@ -0,0 +1,18 @@
+export default function FilterCategory({ onSetSelectedCategory }) {
+  const handleCategoryChange = (event) => {
+    onSetSelectedCategory(event.target.value);
+  };
+  return (
+    <div>
+      <select onChange={handleCategoryChange}>
+        <option value="">All</option>
+        <option value="Salary">Salary</option>
+        <option value="Hobby">Hobby</option>
+        <option value="Food">Food</option>
+        <option value="Household">Household</option>
+        <option value="Health">Health</option>
+        <option value="Others">Others</option>
+      </select>
+    </div>
+  );
+}
