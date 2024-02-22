fd57e483dda7060c6a3bc9c5c99d75543ff977f3
Eileen Ilsemann <eileenilsemanni@gmail.com>
2024-02-22 16:41:54 +0100

    Correct Spelling

--- components/FilterCategory/FilterCategory.js
+++ components/FilterCategory/FilterCategory.js
@@ -11,7 +11,7 @@ export default function FilterCategory({ onSetSelectedCategory }) {
         <option value="Food">Food</option>
         <option value="Household">Household</option>
         <option value="Health">Health</option>
-        <option value="Others">Others</option>
+        <option value="Other">Others</option>
       </select>
     </div>
   );
