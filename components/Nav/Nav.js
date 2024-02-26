2368158d3d7ecbb595af4043769916cca22c317e
Nadine <152076100+nadine-grantz@users.noreply.github.com>
2024-02-26 12:30:13 +0100

    change Nav.js

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styled from "styled-components";
-
 const NavbarWrapper = styled.div`
   display: flex;
   justify-content: space-around;
@@ -13,30 +12,26 @@ const NavbarWrapper = styled.div`
   height: 50px;
   border-top: solid white 1.5px;
 `;
-
 const NavbarLink = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: ${(props) => (props.$active ? "#ffb228" : "#2d3250")};
-  color: ${(props) => (props.$active ? "#2d3250" : "#ffb228")};
+  background-color: ${(props) => (props.$active ? "#FFB228" : "#2D3250")};
+  color: ${(props) => (props.$active ? "#2D3250" : "#FFB228")};
   font-weight: ${(props) => (props.$active ? "bold" : "normal")};
   height: 100%;
   width: 100%;
   padding: 15px 0 0 0;
   border-radius: 5px;
 `;
-
 const StyledLink = styled.div`
   width: 100%;
   position: relative;
   flex-direction: column;
   align-items: center;
 `;
-
 export default function Navbar() {
   const router = useRouter();
-
   // "/" is our "Homepage"
   return (
     <NavbarWrapper>
@@ -44,13 +39,6 @@ export default function Navbar() {
         <Link href="/">
           <NavbarLink $active={router.pathname === "/"}>
             Transactions
-          </NavbarLink>
-        </Link>
-      </StyledLink>
-      <StyledLink>
-        <Link href="/dashboard">
-          <NavbarLink $active={router.pathname === "/dashboard"}>
-            Dashboard
           </NavbarLink>
         </Link>
       </StyledLink>
