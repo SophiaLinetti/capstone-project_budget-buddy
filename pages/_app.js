a69ff465267fc98b30d4c9c12c3eb08753190858
Nadine <152076100+nadine-grantz@users.noreply.github.com>
2024-02-26 12:30:53 +0100

    change app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,26 +3,21 @@ import { initialTransactions } from "@/ressources/data";
 import { v4 as uuidv4 } from "uuid";
 import useLocalStorageState from "use-local-storage-state";
 import { useState } from "react";
-
 export default function App({ Component, pageProps }) {
   const [transactions, setTransactions] = useLocalStorageState("transactions", {
     defaultValue: initialTransactions,
   });
-
   function handleAddTransaction(newTransaction) {
     setTransactions((transactions) => [
-      { ...newTransaction, id: uuidv4(), internalGoalAllocation: "no" },
+      { ...newTransaction, id: uuidv4() },
       ...transactions,
     ]);
-    console.log(transactions)
   }
-
   function handleDeleteTransaction(id) {
     setTransactions((transactions) =>
       transactions.filter((transaction) => transaction.id !== id)
     );
   }
-
   return (
     <>
       <GlobalStyle />
