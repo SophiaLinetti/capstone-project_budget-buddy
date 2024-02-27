34b32636c2f824d9f06bbd889c341258ab4c046e
Sophia <151823139+SophiaZett@users.noreply.github.com>
2024-02-27 14:48:15 +0100

    add percentage and progressbar styling

--- components/ProgressBar/ProgressBar.js
+++ components/ProgressBar/ProgressBar.js
@@ -2,17 +2,23 @@ import styled from "styled-components";
 
 export default function ProgressBar({ savedAmount, goalAmount }) {
   const progress = Math.min((savedAmount / goalAmount) * 100, 100);
+  const roundedProgress = Math.round(progress);
 
   return (
-    <ProgressWrapper>
-      <ProgressInput $progress={progress}></ProgressInput>
-    </ProgressWrapper>
+    <div>
+      <ProgressWrapper>
+        <ProgressInput $progress={progress}></ProgressInput>
+        <Percentage>{roundedProgress} %</Percentage>
+      </ProgressWrapper>
+    </div>
   );
 }
 
 const ProgressWrapper = styled.div`
   width: 100%;
   background-color: #e0e0de;
+  position: relative;
+  height: 20px;
 `;
 
 const ProgressInput = styled.div`
@@ -20,4 +26,19 @@ const ProgressInput = styled.div`
   width: ${({ $progress }) => $progress}%;
   background-color: ${({ $progress }) =>
     $progress === 100 ? "#4caf50" : "#2196f3"};
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Percentage = styled.div`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  color: black;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 `;
