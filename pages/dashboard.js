d2a2fd9dedb772a36727ffc193af0fb9868c044c
Seb <amos.sebastian@gmail.com>
2024-02-28 23:38:54 +0100

    dashboard adjust

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,16 +2,18 @@ import Navbar from "@/components/Nav/Nav";
 import { StyledHeading, FilterButtons } from "@/styles";
 import styled from "styled-components";
 import DoughnutComponent from "@/components/Doughnut/Doughnut";
-
+import useSWR, {mutate} from "swr";
 
 const StyledText = styled.p`
   padding-top: 60px;
 `;
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-
-export default function Dashboard({ transactions }) {
-
+export default function Dashboard({}) {
+  const { data: transactions, error } = useSWR('/api/transactions', fetcher);
+  if (error) return <div>Failed to load transactions</div>;
+  if (!transactions) return <div>Loading...</div>;
 
   return (
     <>
