2383bc9447d9acc261ce16f78c7e615928c8de37
Seb <amos.sebastian@gmail.com>
2024-02-29 10:27:38 +0100

    fix

--- db/models/Transaction.js
+++ db/models/Transaction.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
   date: String,
@@ -9,15 +9,14 @@ const transactionSchema = new mongoose.Schema({
   internalGoalAllocation: String,
 });
 
-// mongoose date fix. DONT TOUCH! And yes, i know about the Date function, its f*
-transactionSchema.set('toJSON', {
+// mongoose date fix
+transactionSchema.set("toJSON", {
   transform: (doc, ret) => {
     ret.date = ret.date.toString();
     return ret;
-  }
-})
+  },
+});
 
-
-
-module.exports = mongoose.models.Transaction || mongoose.model('Transaction', transactionSchema);
-
+module.exports =
+  mongoose.models.Transaction ||
+  mongoose.model("Transaction", transactionSchema);
